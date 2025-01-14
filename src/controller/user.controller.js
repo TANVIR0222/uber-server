426c7d159745ce33696b1ba16eb81abfaedf3681
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-01-14 23:47:53 +0600

    user auth

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,6 +3,12 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/AsyncHandler.js";
 
+// cookies optins 
+const options = {
+    httpOnly: true,
+    secure : true,
+}
+
 // user register
 export const userRegister = asyncHandler(async (req, res) => {
   try {
@@ -31,11 +37,10 @@ export const userRegister = asyncHandler(async (req, res) => {
       lastname,
     });
 
-
     // cheack if user is created
     const cretateUser = await UserModel.findOne({ _id: user._id });
-    if(!cretateUser){
-        return res
+    if (!cretateUser) {
+      return res
         .status(401)
         .json(new ApiResponse(401, user, " User not found! try again"));
     }
@@ -49,3 +54,56 @@ export const userRegister = asyncHandler(async (req, res) => {
       .json(new ApiResponse(500, null, error?.message || error));
   }
 });
+
+//  user login
+export const userLogin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  
+    //  check if all fields are filled
+  if (![email ||  password]) {
+    return res
+      .status(404)
+      .json(new ApiResponse(404, null, " Please fill in all fields"));
+  }
+
+  // check if email is all ready in use
+  const user = await UserModel.findOne({email}).select('+password')  
+  if (!user) {
+    return res
+      .status(401)
+      .json(new ApiResponse(401, null, "User not found! try again"));
+  }
+
+  // check if password is correct
+  const isMatchPassword = await user.isPasswordCorrect(password);
+  if (!isMatchPassword) {
+    return res
+      .status(401)
+      .json(new ApiResponse(401, null, "Invalid password! try again"));
+    }
+
+    // generate token
+    const token = await user.generateAccessToken();
+
+    res.status(200).cookie("accessToken" , token , options)
+
+    //
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {id : user._id , token} , "User login successfull"));
+    
+
+});
+
+
+//  user logout
+export const logoutUser = asyncHandler(async (req,res) => {
+
+    // remove token
+    const userid = req.userId;
+
+    res.status(200)
+    .clearCookie("accessToken"  , options)
+    .json(new ApiResponse(200, {} , "user logout success full "));
+
+})
--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { userRegister } from "../controller/user.controller.js";
+import { logoutUser, userLogin, userRegister } from "../controller/user.controller.js";
+import { verifyToken } from "../middleware/auth.middleware.js";
 
 const router = Router();
 router.route('/register').post(userRegister)
+router.route('/login').post( userLogin)
+router.route('/logout').post( logoutUser)
 
 
 export default router;  //export the router to use it in other files
